f0f30c5dd5bcff177e461a44027c2452101b56c5
mihoz-fe <mihozfe@gmail.com>
2019-08-09 10:26:18 +0300

    Init

---
+++ src/js/simple_product_tour.js
@@ -0,0 +1,96 @@
+Window.onload = function () {
+    if (document.body.classList.contains('simple-product-tour')) {
+
+        var tour = document.createElement('div');
+        tour.className ='tour overlay';
+        tour.innerHTML = '<div class="tour-notify" id="notify"><div class="tour-notify-actions"><button id="btnNext">next</button><button id="btnFinish" hidden>finish</button></div></div>';
+        document.body.append(tour);
+
+
+        var tourOverlay = document.querySelector('.overlay.tour'),
+            tourList = document.querySelectorAll('.point'),
+            sortedList = [],
+            i = 0,
+            notifyList = [
+                '1. notify one',
+                '2. notify two',
+                '3. notify three'
+            ];
+
+        tourList.forEach(function (item) {
+            sortedList.push({ item });
+        });
+
+        sortedList.sort(function (a, b) {
+            if (a.item.dataset.step > b.item.dataset.step) {
+                return 1;
+            }
+            if (a.item.dataset.step < b.item.dataset.step) {
+                return -1;
+            }
+            return 0;
+        });
+
+        function showNext() {
+            if (sortedList.length > 2 && i > 0) {
+                sortedList[i - 1].item.classList.remove('tour-point-show');
+            }
+            if (i < sortedList.length) {
+                sortedList[i].item.classList.add('tour-point-show');
+            }
+            insertNotify(notify, notifyList[i]);
+            position(notify, sortedList[i].item);
+        };
+
+        function getCoordinates(anch) {
+            var coord = anch.getBoundingClientRect();
+            return {
+                top: coord.top + pageYOffset,
+                left: coord.left + pageXOffset,
+                anchHeight: anch.offsetHeight,
+                anchWidth: anch.offsetWidth
+            };
+        };
+
+        function position(el, anch) {
+            var anchCoord = getCoordinates(anch),
+                anchSizes = '';
+            el.style.left = anchCoord.left + anchCoord.anchWidth + 10 + "px";
+            el.style.top = anchCoord.top + anchCoord.anchHeight / 2 - 15 + "px";
+        };
+
+        function insertNotify(el, txt) {
+            el.firstChild.data = txt;
+        };
+
+        function next() {
+
+            i++;
+            console.log(i)
+            if (i === sortedList.length) {
+                sortedList[i - 1].item.classList.remove('tour-point-show');
+                btnNext.innerHTML = "next";
+                btnFinish.setAttribute('hidden', true);
+                i = 0;
+                showNext();
+                return
+            };
+
+            showNext();
+
+            if (i === sortedList.length - 1) {
+                this.innerHTML = "once more";
+                btnFinish.removeAttribute('hidden');
+            };
+        };
+
+        showNext();
+
+        btnNext.onclick = next;
+
+        btnFinish.onclick = function () {
+            next();
+            tourOverlay.style.display = "none";
+        };
+    };
+};
